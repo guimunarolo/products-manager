ab3ee32b197dbf12c17e1b17131f9fa3a761eaa3
Guilherme Munarolo <guimunarolo@hotmail.com>
2020-10-12 14:23:55 -0300

    Created base gRPC server

--- calculator-service/go.mod
+++ calculator-service/go.mod
@@ -1,3 +1,10 @@
 module github.com/guimunarolo/products-manage/calculator-service
 
 go 1.15
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/hashicorp/go-hclog v0.14.1
+	google.golang.org/grpc v1.33.0
+	google.golang.org/protobuf v1.25.0
+)
--- calculator-service/main.go
+++ calculator-service/main.go
@@ -1,19 +1,32 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net/http"
+	"fmt"
+	"net"
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	protos "github.com/guimunarolo/products-manage/calculator-service/protos/calculator"
+	"github.com/guimunarolo/products-manage/calculator-service/servers"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
-	// Hello world, the web server
+	log := hclog.Default()
+	gs := grpc.NewServer()
+	c := server.NewCalculator(log)
 
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, world!\n")
+	protos.RegisterCalculatorServer(gs, c)
+	reflection.Register(gs)
+
+	port := 9000
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Error("Unable to create listener", "error", err)
+		os.Exit(1)
 	}
 
-	http.HandleFunc("/hello", helloHandler)
-    log.Println("Listing for requests at http://localhost:9000/hello")
-	log.Fatal(http.ListenAndServe(":9000", nil))
+	log.Info("gRPC Server running", "port", port)
+	gs.Serve(l)
 }
