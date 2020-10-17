a8596a493581cde208a09393557ae4fcbfee7a47
Guilherme Munarolo <guimunarolo@hotmail.com>
2020-10-16 21:19:32 -0300

    applied products discounts for user logic on /product view

--- products-api/products_api/views.py
+++ products-api/products_api/views.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 
 from .managers import ProductManager
 from .models import Product
@@ -8,6 +8,9 @@ from .models import Product
 router = APIRouter()
 
 
-@router.get("/products/", response_model=List[Product])
-async def list_products():
+@router.get("/product", response_model=List[Product])
+async def list_products(x_user_id: Optional[str] = Header(None)):
+    if x_user_id:
+        return await ProductManager.get_all_with_discount(user_id=x_user_id)
+
     return await ProductManager.get_all()
--- products-api/tests/test_views.py
+++ products-api/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import mock\
+
 import pytest
 from fastapi import status
 
@@ -8,7 +10,7 @@ pytestmark = pytest.mark.asyncio
 
 
 async def test_list_products_without_stored_products(client, db_connection, product_data):
-    response = await client.get("/products/")
+    response = await client.get("/product")
     response_data = response.json()
 
     assert response.status_code == status.HTTP_200_OK
@@ -20,7 +22,7 @@ async def test_list_products_with_stored_products(client, db_connection, product
     del product_data["id"]
     await ProductManager.create(product_data)
 
-    response = await client.get("/products/")
+    response = await client.get("/product")
     response_data = response.json()
 
     assert response.status_code == status.HTTP_200_OK
@@ -29,3 +31,27 @@ async def test_list_products_with_stored_products(client, db_connection, product
     assert response_data[0]["description"] == product_data["description"]
     assert response_data[0]["price_in_cents"] == product_data["price_in_cents"]
     assert response_data[0]["discount"] == {}
+
+
+@database.transaction(force_rollback=True)
+@mock.patch("products_api.managers.CalculatorClient.get_product_discount")
+async def test_list_products_with_given_used_id(
+    get_product_discount_mock, client, db_connection, product_data, user_id
+):
+    del product_data["id"]
+    await ProductManager.create(product_data)
+    
+    discount = {"pct": 0.1, "value_in_cents": 100}
+    get_product_discount_mock.return_value = discount
+    response = await client.get("/product", headers={"X-USER-ID": user_id})
+    response_data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert len(response_data) == 1
+    assert response_data[0]["title"] == product_data["title"]
+    assert response_data[0]["description"] == product_data["description"]
+    assert response_data[0]["price_in_cents"] == product_data["price_in_cents"]
+    assert response_data[0]["discount"] == discount
+    get_product_discount_mock.assert_called_once_with(
+        user_id=user_id, product_id=response_data[0]["id"]
+    )
